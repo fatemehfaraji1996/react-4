21b2975022ae0f176e0e6452541f699b266a882d
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-05-28 21:29:29 +0330

    add loding

--- src/components/postList.jsx
+++ src/components/postList.jsx
@@ -2,22 +2,23 @@ import { useState, useEffect } from 'react';
 function PoastList() {
         const url = 'https://jsonplaceholder.typicode.com/posts'
        const [data,setdata]= useState([])
-       const [loading,setloading]= useState(true)
+       const [loading,setLoading]= useState(true)
        
     useEffect(()=>{
         fetch(url)
         .then(res=>res.json())
         .then((data)=>{
-            console.log(data)
+            // console.log(data)
             setdata(data)
+            setLoading(false)
         })  
     },[])
     return(
         <>
-        {loading ? <p className='loader'></p> :
+        {loading ? <p className='loader'>loading</p> :
         <ul>
           {data.map((x,i)=>{
-            return  <li key={i}>{x.title}</li>
+            <li key={i}>{x.title}</li>
             })}
         </ul>
         }
