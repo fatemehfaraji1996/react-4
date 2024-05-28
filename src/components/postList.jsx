06872c00c6dd92b33d969f3b988327b9a917217c
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-05-28 21:19:30 +0330

    loading

--- src/components/postList.jsx
+++ src/components/postList.jsx
@@ -2,26 +2,25 @@ import { useState, useEffect } from 'react';
 function PoastList() {
         const url = 'https://jsonplaceholder.typicode.com/posts'
        const [data,setdata]= useState([])
+       const [loading,setloading]= useState(true)
+       
     useEffect(()=>{
         fetch(url)
         .then(res=>res.json())
         .then((data)=>{
             console.log(data)
-            
             setdata(data)
         })  
     },[])
     return(
         <>
+        {loading ? <p className='loader'></p> :
         <ul>
-          {
-            data.map((x,i)=>{
-            return    <li key={i}>{x.title}</li>
-            })
-          }
-
+          {data.map((x,i)=>{
+            return  <li key={i}>{x.title}</li>
+            })}
         </ul>
-        
+        }
         </>
     )
 }
